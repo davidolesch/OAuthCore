dd98cd44176ebd3b815021a23c05a41a98a01acd
David Olesch <david@soulemobile.com>
2013-06-05 12:53:06 -0500

    moved the logic to OAuthorizationHeaderWithCallbackAndVerifier and made OAuthorizationHeaderWithCallback just call OAuthorizationHeaderWithCallbackAndVerifier when an Authorization Header without a verifier is needed.

--- OAuthCore.m
+++ OAuthCore.m
@@ -32,86 +32,7 @@ NSString *OAuthorizationHeader(NSURL *url, NSString *method, NSData *body, NSStr
 }
 
 NSString *OAuthorizationHeaderWithCallback(NSURL *url, NSString *method, NSData *body, NSString *_oAuthConsumerKey, NSString *_oAuthConsumerSecret, NSString *_oAuthToken, NSString *_oAuthTokenSecret, NSString *_oAuthCallback) {
-	NSString *_oAuthNonce = [NSString ab_GUID];
-	NSString *_oAuthTimestamp = [NSString stringWithFormat:@"%d", (int)[[NSDate date] timeIntervalSince1970]];
-	NSString *_oAuthSignatureMethod = @"HMAC-SHA1";
-	NSString *_oAuthVersion = @"1.0";
-	
-	NSMutableDictionary *oAuthAuthorizationParameters = [NSMutableDictionary dictionary];
-	[oAuthAuthorizationParameters setObject:_oAuthNonce forKey:@"oauth_nonce"];
-	[oAuthAuthorizationParameters setObject:_oAuthTimestamp forKey:@"oauth_timestamp"];
-	[oAuthAuthorizationParameters setObject:_oAuthSignatureMethod forKey:@"oauth_signature_method"];
-	[oAuthAuthorizationParameters setObject:_oAuthVersion forKey:@"oauth_version"];
-	[oAuthAuthorizationParameters setObject:_oAuthConsumerKey forKey:@"oauth_consumer_key"];
-	if(_oAuthToken)
-		[oAuthAuthorizationParameters setObject:_oAuthToken forKey:@"oauth_token"];
-	if (_oAuthCallback)
-		[oAuthAuthorizationParameters setObject:_oAuthCallback forKey:@"oauth_callback"];
-
-	// get query and body parameters
-	NSDictionary *additionalQueryParameters = [NSURL ab_parseURLQueryString:[url query]];
-	NSDictionary *additionalBodyParameters = nil;
-	if(body) {
-		NSString *string = [[[NSString alloc] initWithData:body encoding:NSUTF8StringEncoding] autorelease];
-		if(string) {
-			additionalBodyParameters = [NSURL ab_parseURLQueryString:string];
-		}
-	}
-	
-	// combine all parameters
-	NSMutableDictionary *parameters = [[oAuthAuthorizationParameters mutableCopy] autorelease];
-	if(additionalQueryParameters) [parameters addEntriesFromDictionary:additionalQueryParameters];
-	if(additionalBodyParameters) [parameters addEntriesFromDictionary:additionalBodyParameters];
-	
-	// -> UTF-8 -> RFC3986
-	NSMutableDictionary *encodedParameters = [NSMutableDictionary dictionary];
-	for(NSString *key in parameters) {
-		NSString *value = [parameters objectForKey:key];
-		[encodedParameters setObject:[value ab_RFC3986EncodedString] forKey:[key ab_RFC3986EncodedString]];
-	}
-	
-	NSArray *sortedKeys = [[encodedParameters allKeys] sortedArrayUsingFunction:SortParameter context:encodedParameters];
-	
-	NSMutableArray *parameterArray = [NSMutableArray array];
-	for(NSString *key in sortedKeys) {
-		[parameterArray addObject:[NSString stringWithFormat:@"%@=%@", key, [encodedParameters objectForKey:key]]];
-	}
-	NSString *normalizedParameterString = [parameterArray componentsJoinedByString:@"&"];
-	
-	NSString *normalizedURLString;
-	if([url port] == nil) {
-		normalizedURLString = [NSString stringWithFormat:@"%@://%@%@", [url scheme], [url host], [url ab_actualPath]];
-	} else {
-		normalizedURLString = [NSString stringWithFormat:@"%@://%@:%@%@", [url scheme], [url host], [url port], [url ab_actualPath]];
-	}
-	
-	NSString *signatureBaseString = [NSString stringWithFormat:@"%@&%@&%@",
-									 [method ab_RFC3986EncodedString],
-									 [normalizedURLString ab_RFC3986EncodedString],
-									 [normalizedParameterString ab_RFC3986EncodedString]];
-	
-	NSString *key = [NSString stringWithFormat:@"%@&%@",
-					 [_oAuthConsumerSecret ab_RFC3986EncodedString],
-					 [_oAuthTokenSecret ab_RFC3986EncodedString] ?: @""];
-	
-	NSData *signature = HMAC_SHA1(signatureBaseString, key);
-	NSString *base64Signature = [signature base64EncodedString];
-	
-	NSMutableDictionary *authorizationHeaderDictionary = [[oAuthAuthorizationParameters mutableCopy] autorelease];
-	[authorizationHeaderDictionary setObject:base64Signature forKey:@"oauth_signature"];
-	
-	NSMutableArray *authorizationHeaderItems = [NSMutableArray array];
-	for(NSString *key in authorizationHeaderDictionary) {
-		NSString *value = [authorizationHeaderDictionary objectForKey:key];
-		[authorizationHeaderItems addObject:[NSString stringWithFormat:@"%@=\"%@\"",
-											 [key ab_RFC3986EncodedString],
-											 [value ab_RFC3986EncodedString]]];
-	}
-	
-	NSString *authorizationHeaderString = [authorizationHeaderItems componentsJoinedByString:@", "];
-	authorizationHeaderString = [NSString stringWithFormat:@"OAuth %@", authorizationHeaderString];
-	
-	return authorizationHeaderString;
+	return OAuthorizationHeaderWithCallbackAndVerifier(url,method,body,_oAuthConsumerKey,_oAuthConsumerSecret,_oAuthToken,_oAuthTokenSecret,_oAuthCallback,nil);
 }
 
 NSString *OAuthorizationHeaderWithCallbackAndVerifier(NSURL *url, NSString *method, NSData *body, NSString *_oAuthConsumerKey, NSString *_oAuthConsumerSecret, NSString *_oAuthToken, NSString *_oAuthTokenSecret, NSString *_oAuthCallback, NSString *_oAuthVerifier) {
@@ -124,7 +45,8 @@ NSString *OAuthorizationHeaderWithCallbackAndVerifier(NSURL *url, NSString *meth
 	[oAuthAuthorizationParameters setObject:_oAuthNonce forKey:@"oauth_nonce"];
 	[oAuthAuthorizationParameters setObject:_oAuthTimestamp forKey:@"oauth_timestamp"];
 	[oAuthAuthorizationParameters setObject:_oAuthSignatureMethod forKey:@"oauth_signature_method"];
-    [oAuthAuthorizationParameters setObject:_oAuthVerifier forKey:@"oauth_verifier"];
+    if(_oAuthVerifier)
+        [oAuthAuthorizationParameters setObject:_oAuthVerifier forKey:@"oauth_verifier"];
 	[oAuthAuthorizationParameters setObject:_oAuthVersion forKey:@"oauth_version"];
 	[oAuthAuthorizationParameters setObject:_oAuthConsumerKey forKey:@"oauth_consumer_key"];
 	if(_oAuthToken)
